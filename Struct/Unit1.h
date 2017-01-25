37410dee6f30d2a59318d8ad870f8919d88e7ad3
LizaSh <lizash605@gmail.com>
2017-01-25 21:15:40 +0200

    Desinged form

--- Struct/Unit1.h
+++ Struct/Unit1.h
@@ -11,6 +11,18 @@
 class TForm1 : public TForm
 {
 __published:	// IDE-managed Components
+	TLabel *Label1;
+	TLabel *Label2;
+	TLabel *Label3;
+	TLabel *Label4;
+	TEdit *Edit1;
+	TEdit *Edit2;
+	TEdit *Edit3;
+	TEdit *Edit4;
+	TMemo *Memo1;
+	TButton *addBook;
+	TButton *info;
+	TButton *Button2;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
